be61a326062fd6bc6ccb11de581170b65064cf80
Makcim80 <makcim80@mail.ru>
2023-02-20 22:40:19 +0300

    feat: implement task in src/guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.round((this.max + this.min) / 2);
     }
 
     lower() {
-
+        this.max = this.guess();
     }
 
     greater() {
-
+        this.min = this.guess();
     }
 }
 
